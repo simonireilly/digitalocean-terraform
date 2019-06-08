f48b83567106299ec22d34af3cec2c6d9d124b86
simonireilly@gmail.com <simonireilly@gmail.com>
2019-06-08 12:42:40 +0100

    Working load blancer with nginx and haproxy

--- web2.tf
+++ web2.tf
@@ -17,7 +17,10 @@ resource "digitalocean_droplet" "web2" {
   provisioner "remote-exec" {
     inline = [
       "sudo apt-get update",
-      "sudo apt-get -y install nginx"
+      "sudo apt-get -y install nginx",
+      "export HOSTNAME=$(curl -s http://169.254.169.254/metadata/v1/hostname)",
+      "export PUBLIC_IPV4=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)",
+      "echo Hello from Droplet $HOSTNAME, with IP Address: $PUBLIC_IPV4 > /var/www/html/index.nginx-debian"
     ]
   }
 }
